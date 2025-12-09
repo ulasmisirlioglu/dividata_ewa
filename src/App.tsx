b66f93d29a21b9f45afa87ff65906152714c732d
Ulas Misirlioglu <ulasmisirlioglu@example.com>
2025-12-09 02:28:44 +0100

    Add ScrollToTop for better UX

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,20 @@ import { Onboarding } from './pages/Onboarding';
 import { AnalogProcess } from './pages/AnalogProcess';
 import { Evaluation } from './pages/Evaluation';
 import { Results } from './pages/Results';
+import { ScrollToTop } from './components/ScrollToTop';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Onboarding />} />
-      <Route path="/analog-process" element={<AnalogProcess />} />
-      <Route path="/ewa-evaluation" element={<Evaluation />} />
-      <Route path="/results" element={<Results />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Onboarding />} />
+        <Route path="/analog-process" element={<AnalogProcess />} />
+        <Route path="/ewa-evaluation" element={<Evaluation />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </>
   );
 }
 
 export default App;
-
